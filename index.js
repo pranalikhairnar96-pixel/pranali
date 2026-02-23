1e81a6bbb065870ec707014eb2541f00b5458ce5
pranalikhairnar96-pixel <pranalikhairnar96@gmail.com>
2026-02-23 13:42:32 +0530

    my app
    access the app

---
+++ index.js
@@ -0,0 +1,33 @@
+let num=[10,20,30,40,50];
+console.log("i want to print array element");
+for(let i=0;i<num.length;i++){
+    console.log(num[i]);
+}
+num.push(60);
+console.log("after adding new element")
+console.log(num[5])
+
+let age=18;
+if(age>=18){
+    console.log("he is eligible to vote");
+}
+else{
+    console.log("not eligible to vote");
+}
+
+let percentage = 95;
+if(percentage>90)
+{
+    console.log("a");
+}
+else if(percentage>80)
+{
+    console.log("b");
+}
+else{
+    console.log("fail");    
+}
+let odd=[1,3,5,7,9];
+for(let i =1;i<=9;i=i+1){
+    console.log(i);
+}
